c6dcc3f3b0f3d58eedd5844908b0f2d63e504edb
Jordan <danipavlev@gmail.com>
2024-03-23 10:35:37 +0200

    adde pages, routes, css theme

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -11,6 +11,12 @@ class CalendarController extends AbstractController
     #[Route('/calendar', name: 'app_calendar')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        if(!$user) {
+            $this->redirect('asd');
+        }
+
         return $this->render('calendar/index.html.twig', [
             'controller_name' => 'CalendarController',
         ]);
