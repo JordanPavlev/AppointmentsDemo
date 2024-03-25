48ccfc2b30ac1f9ddb1a96d140361b698ebe1d06
Jordan <j.pavlev@stynex.com>
2024-03-25 17:29:40 +0200

    Infinite redirect fix

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,9 +19,9 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        if($lastUsername != null) {
-            return new RedirectResponse('/');
-        }
+        // if($lastUsername != null) {
+        //     return new RedirectResponse('/');
+        // }
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
--- src/EventSubscriber/AuthenticationSubscriber.php
+++ src/EventSubscriber/AuthenticationSubscriber.php
@@ -8,22 +8,25 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Event\AuthenticationFailureEvent;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 class AuthenticationSubscriber implements EventSubscriberInterface
 {
     private $security;
     private $urlGenerator;
+    private $authenticationUtils;
 
-    public function __construct(Security $security, UrlGeneratorInterface $urlGenerator)
+    public function __construct(Security $security, UrlGeneratorInterface $urlGenerator, AuthenticationUtils $authenticationUtils)
     {
         $this->security = $security;
         $this->urlGenerator = $urlGenerator;
+        $this->authenticationUtils = $authenticationUtils;
     }
 
     public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
-
+       
         // Check if the user is not authenticated and is accessing / or /product
         if (!$this->security->isGranted('IS_AUTHENTICATED_FULLY') && 
             ($request->getPathInfo() !== '/login')) {
@@ -32,6 +35,11 @@ class AuthenticationSubscriber implements EventSubscriberInterface
             $event->setResponse($response);
         }
 
+    }
+    private function isAuthenticationFailure(): bool
+    {
+        // Check if there was an authentication error (failed login attempt)
+        return $this->authenticationUtils->getLastAuthenticationError() !== null;
     }
     
 
